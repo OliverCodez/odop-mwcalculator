55e043d7895bdd8d527d7f1f349d53533dc705ae
Oliver <oliverw97@proton.me>
2023-03-24 16:56:44 -0600

    Debug

--- client/src/components/DesignTable.jsx
+++ client/src/components/DesignTable.jsx
@@ -23,6 +23,7 @@ class DesignTable extends Component {
                 </Row>
                 <div id="reset-button">
                     <button className="bg-neutral-500 text-white hover:bg-neutral-600 text-md font-semibold antialiased rounded py-12 px-16 text-center duration block" onClick={ () => {
+                        document.getElementById( 'nvuriv_Free_OD' ).value = '0';
                         console.log('clicked reset');
                     } }>Reset</button>
                 </div>
