9debffb8eab2fd4f835b8a47d185e1c63911f18a
Oliver <oliverw97@proton.me>
2023-03-24 14:54:03 -0600

    Debug

--- client/src/designtypes/Spring/Compression/FooterReport.jsx
+++ client/src/designtypes/Spring/Compression/FooterReport.jsx
@@ -32,7 +32,7 @@ class Report extends ReportBase {
                     <HubspotForm
                         portalId='8642978'
                         formId='e2d615a6-2965-487e-85da-c79dc8113a9d'
-                        onFormSubmit={() => console.log('Submit!')}
+                        onSubmit={() => console.log('Submit!')}
                         onFormSubmitted={() => document.getElementById('report_Inner').classList.remove('hide-soft')}
                         onReady={(form) => console.log('Form Loaded')}
                     />
