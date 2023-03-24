b67f09b4e1606356b71566a238cb5a12ad08c1ae
Oliver <oliverw97@proton.me>
2023-03-24 12:54:20 -0600

    Debugging

--- client/src/designtypes/Spring/Compression/Calculator.jsx
+++ client/src/designtypes/Spring/Compression/Calculator.jsx
@@ -280,7 +280,7 @@ export class Calculator extends ReportBase {
                         portalId='8642978'
                         formId='e2d615a6-2965-487e-85da-c79dc8113a9d'
                         onSubmit={() => console.log('Submit!')}
-                        onReady={(form) => reportEl.classList.toggle('hide-soft')}
+                        onReady={(form) => console.log(reportEl.classList)}
                     />
                 </div>
            </Container>
