3b4c1751c3f603908998a5807981796d2dd48ca4
Oliver <oliverw97@proton.me>
2023-03-28 11:00:49 -0600

    debug

--- client/src/components/Value.jsx
+++ client/src/components/Value.jsx
@@ -16,22 +16,23 @@ class Value extends Component {
     
     render() {
 //        console.log('In Value.render this=',this);
-        var id_zero = 'Deflection_Solid OD_Solid ID_Solid',
+        var classNames = 'text-right',
+            id_zero = 'Deflection_Solid OD_Solid ID_Solid',
             id_nan = 'OD_1 ID_1 OD_2 ID_2 Static_FS_1';
         if ( id_zero.includes( this.props.id ) ) {
-            className = className + ' clear-val';
+            classNames = classNames + ' clear-val';
         }
         if ( id_nan.includes( this.props.id ) ) {
-            className = className + ' clear-nan';
+            classNames = classNames + ' clear-nan';
         }
         return (
             <>
                 <td className={"align-middle " + (this.props.className !== undefined ? this.props.className : '')}>
                     <InputGroup>
                         {typeof this.props.value === 'number' ?
-                            <Form.Control id={'v_'+this.props.id} type="text" disabled={true} className="text-right" value={this.props.value.toODOPPrecision()} />
+                            <Form.Control id={'v_'+this.props.id} type="text" disabled={true} className={classNames} value={this.props.value.toODOPPrecision()} />
                             :
-                            <Form.Control id={'v_'+this.props.id} type="text" disabled={true} className="text-right" value={this.props.value} />
+                            <Form.Control id={'v_'+this.props.id} type="text" disabled={true} className={classNames} value={this.props.value} />
                         }
                     </InputGroup>
                 </td>
