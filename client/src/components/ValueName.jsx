3df6c7fa6286d9ccde8cdd711ba292829526b466
Oliver <oliverw97@proton.me>
2023-04-11 13:05:48 -0600

    Fixes

--- client/src/components/ValueName.jsx
+++ client/src/components/ValueName.jsx
@@ -5,11 +5,13 @@ import { connect } from 'react-redux';
 class ValueName extends Component {
     
     render() {
+        var appendClass = '';
+        if ( this.props.id == 'max_t1' || this.props.id == 'min_t1' ) appendClass = ' lineheight-tight';
 //        console.log('In ValueName.render this=',this);
         return (
             <td className={"align-middle " + (this.props.className !== undefined ? this.props.className : '')} id={"vn_" + this.props.id}>
                 <OverlayTrigger placement="top" overlay={this.props.tooltip !== undefined && <Tooltip>{this.props.tooltip}</Tooltip>}>
-                    <span className="d-block">{this.props.name}</span>
+                    <span className={"d-block" + appendClass}>{this.props.name}</span>
                 </OverlayTrigger>
             </td>
         );
