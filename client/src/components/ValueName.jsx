6b95f957cdb8095f044801dfce65a9d09f763517
Oliver <oliverw97@proton.me>
2023-04-11 12:18:15 -0600

    Fixes and debug

--- client/src/components/ValueName.jsx
+++ client/src/components/ValueName.jsx
@@ -6,23 +6,10 @@ class ValueName extends Component {
     
     render() {
 //        console.log('In ValueName.render this=',this);
-        var elName = this.props.name,
-            elDisplay = elName,
-            addtlClass = '';
-        if ( elName == 'Free' ) elDisplay = 'Free Length';
-        if ( elName == 'Solid' ) elDisplay = 'Solid Height';
-        if ( elName == '1' ) {
-            addtlClass = ' lineheight-tight';
-            elDisplay = 'Min<br>Operating Load';
-        }
-        if ( elName = '2' ) {
-            addtlClass = ' lineheight-tight';
-            elDisplay = 'Max<br>Operating Load';
-        }
         return (
-            <td className={"align-middle " + (this.props.className !== undefined ? this.props.className : '')} id={"vn_" + (elName)}>
+            <td className={"align-middle " + (this.props.className !== undefined ? this.props.className : '')} id={"vn_" + (this.props.name)}>
                 <OverlayTrigger placement="top" overlay={this.props.tooltip !== undefined && <Tooltip>{this.props.tooltip}</Tooltip>}>
-                    <span className={"d-block" + addtlClass}>{elDisplay}</span>
+                    <span className="d-block">{this.props.name}</span>
                 </OverlayTrigger>
             </td>
         );
