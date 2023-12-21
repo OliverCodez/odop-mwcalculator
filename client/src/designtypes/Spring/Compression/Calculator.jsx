04cb4290293536c0408d2af889d0b5aaead73517
Oliver <oliverw97@proton.me>
2023-12-20 18:31:34 -0700

    Fixes and Debugs

--- client/src/designtypes/Spring/Compression/Calculator.jsx
+++ client/src/designtypes/Spring/Compression/Calculator.jsx
@@ -24,8 +24,8 @@ export class Calculator extends ReportBase {
         return (
             <Container>
                 <script>
-                    console.log('safeload::' + this.safe_load);
-                    sessionStorage.setItem( 'safe_load', this.safe_load );
+                    {console.log('safeload::' + this.safe_load)}
+                    {sessionStorage.setItem( 'safe_load', this.safe_load )}
                 </script>
                 <Row>
                     <table className="report-table report-table-1">
